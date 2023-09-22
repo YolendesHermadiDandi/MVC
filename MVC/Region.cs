9430b53a5a8d18adcf631fc0be03c2dfd77dd0ac
YolendesHermadiDandi <marselius306@gmail.com>
2023-09-22 10:11:10 +0700

    mvc update

---
+++ MVC/Region.cs
@@ -0,0 +1,136 @@
+﻿namespace BasicConnectivity;
+
+public class Region
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public override string ToString()
+    {
+        return $"{Id} - {Name}";
+    }
+
+    public List<Region> GetAll()
+    {
+        var regions = new List<Region>();
+
+        using var connection = Provider.GetConnection();
+        using var command = Provider.GetCommand();
+
+        command.Connection = connection;
+        command.CommandText = "SELECT * FROM regions";
+
+        try
+        {
+            connection.Open();
+
+            using var reader = command.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    regions.Add(new Region {
+                        Id = reader.GetInt32(0),
+                        Name = reader.GetString(1)
+                    });
+                }
+                reader.Close();
+                connection.Close();
+                
+                return regions;
+            }
+            reader.Close();
+            connection.Close();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+
+        return new List<Region>();
+    }
+
+    public Region GetById(int id)
+    {
+        var region = new Region();
+        using var connection = Provider.GetConnection();
+        using var command = Provider.GetCommand();
+
+        command.Connection = connection;
+        command.CommandText = "SELECT * FROM regions where id = @id";
+        command.Parameters.Add(Provider.SetParameter("@id", id));
+
+        try
+        {
+            connection.Open();
+
+            using var reader = command.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                reader.Read();
+
+                region.Id = reader.GetInt32(0);
+                region.Name = reader.GetString(1);
+            }
+            reader.Close();
+            connection.Close();
+            
+            return region;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+
+        return new Region();
+    }
+
+    public string Insert(Region region)
+    {
+        using var connection = Provider.GetConnection();
+        using var command = Provider.GetCommand();
+
+        command.Connection = connection;
+        command.CommandText = "INSERT INTO regions VALUES (@name);";
+
+        try
+        {
+            command.Parameters.Add(Provider.SetParameter("@name", region.Name));
+
+            connection.Open();
+            using var transaction = connection.BeginTransaction();
+            try
+            {
+                command.Transaction = transaction;
+
+                var result = command.ExecuteNonQuery();
+
+                transaction.Commit();
+                connection.Close();
+
+                return result.ToString();
+            }
+            catch (Exception ex)
+            {
+                transaction.Rollback();
+                return $"Error Transaction: {ex.Message}";
+            }
+        }
+        catch (Exception ex)
+        {
+            return $"Error: {ex.Message}";
+        }
+    }
+
+    public string Update(Region region)
+    {
+        return "";
+    }
+
+    public string Delete(int id)
+    {
+        return "";
+    }
+}
